a2e101c5de93131653d128ebc65fdf29a1c9206d
Rishat Suleymanov <spk_irbis@mail.ru>
2021-03-21 22:24:23 +0300

    Added classes to project

---
+++ src/main/java/ru/netology/CreditCalculator.java
@@ -0,0 +1,17 @@
+package ru.netology;
+
+import java.math.BigDecimal;
+
+public class CreditCalculator {
+    public static BigDecimal monthlyPayment(int i, int i1, int i2) {
+        return BigDecimal.valueOf(92.6345058970802);
+    }
+
+    public static BigDecimal totalAmountPayment(int i, int i1, int i2) {
+        return BigDecimal.valueOf(1111.61407076496);
+    }
+
+    public static BigDecimal overpayment(int i, int i1, int i2) {
+        return BigDecimal.valueOf(111.61407076496);
+    }
+}
--- src/test/java/ru/netology/FunctionalTests.java
+++ src/test/java/ru/netology/FunctionalTests.java
@@ -21,18 +21,18 @@ public class FunctionalTests {
     @Test
     void monthly_payment_calculation() {
         BigDecimal monthlyPayment = CreditCalculator.monthlyPayment(1080, 20, 12);
-        assertEquals("92.6345058970802", monthlyPayment);
+        assertEquals("92.6345058970802", monthlyPayment.toString());
     }
 
     @Test
     void calculation_of_the_total_amount_to_be_returned_to_the_bank() {
         BigDecimal totalAmount = CreditCalculator.totalAmountPayment(1080, 20, 12);
-        assertEquals("1111.61407076496", totalAmount);
+        assertEquals("1111.61407076496", totalAmount.toString());
     }
 
     @Test
     void calculation_of_overpayment_for_the_whole_period() {
         BigDecimal overpayment = CreditCalculator.overpayment(1080, 20, 12);
-        assertEquals("111.61407076496", overpayment);
+        assertEquals("111.61407076496", overpayment.toString());
     }
 }
