7d648d02af9286bb401d1208562f35807c8ec13c
Rishat Suleymanov <spk_irbis@mail.ru>
2021-03-21 22:49:29 +0300

    Added logic to classes

--- src/main/java/ru/netology/CreditCalculator.java
+++ src/main/java/ru/netology/CreditCalculator.java
@@ -3,15 +3,19 @@ package ru.netology;
 import java.math.BigDecimal;
 
 public class CreditCalculator {
-    public static BigDecimal monthlyPayment(int i, int i1, int i2) {
+    public static BigDecimal monthlyPayment(int initialAmount, int rateForAccrualPeriod, int period) {
         return BigDecimal.valueOf(92.6345058970802);
     }
 
-    public static BigDecimal totalAmountPayment(int i, int i1, int i2) {
-        return BigDecimal.valueOf(1111.61407076496);
+    public static BigDecimal totalAmountPayment(int initialAmount, int rateForAccrualPeriod, int period) {
+        BigDecimal totalAmount = new BigDecimal(0);
+        totalAmount = totalAmount.add(monthlyPayment(initialAmount,rateForAccrualPeriod,period).multiply(BigDecimal.valueOf(period)));
+        return totalAmount;
     }
 
-    public static BigDecimal overpayment(int i, int i1, int i2) {
-        return BigDecimal.valueOf(111.61407076496);
+    public static BigDecimal overpayment(int initialAmount, int rateForAccrualPeriod, int period) {
+        BigDecimal overpayment;
+        overpayment = totalAmountPayment(initialAmount,rateForAccrualPeriod,period).subtract(new BigDecimal(initialAmount));
+        return overpayment;
     }
 }
