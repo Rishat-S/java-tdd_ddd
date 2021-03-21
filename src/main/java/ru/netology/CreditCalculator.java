64bd7e15a6c36ca139a3d96f6ca856ee22413193
Rishat Suleymanov <spk_irbis@mail.ru>
2021-03-21 22:53:29 +0300

    Refactoring tests

--- src/main/java/ru/netology/CreditCalculator.java
+++ src/main/java/ru/netology/CreditCalculator.java
@@ -4,18 +4,22 @@ import java.math.BigDecimal;
 
 public class CreditCalculator {
     public static BigDecimal monthlyPayment(int initialAmount, int rateForAccrualPeriod, int period) {
+
         return BigDecimal.valueOf(92.6345058970802);
     }
 
     public static BigDecimal totalAmountPayment(int initialAmount, int rateForAccrualPeriod, int period) {
         BigDecimal totalAmount = new BigDecimal(0);
-        totalAmount = totalAmount.add(monthlyPayment(initialAmount,rateForAccrualPeriod,period).multiply(BigDecimal.valueOf(period)));
+        totalAmount = totalAmount
+                .add(monthlyPayment(initialAmount, rateForAccrualPeriod, period)
+                        .multiply(BigDecimal.valueOf(period)));
         return totalAmount;
     }
 
     public static BigDecimal overpayment(int initialAmount, int rateForAccrualPeriod, int period) {
         BigDecimal overpayment;
-        overpayment = totalAmountPayment(initialAmount,rateForAccrualPeriod,period).subtract(new BigDecimal(initialAmount));
+        overpayment = totalAmountPayment(initialAmount, rateForAccrualPeriod, period)
+                .subtract(new BigDecimal(initialAmount));
         return overpayment;
     }
 }
--- src/test/java/ru/netology/FunctionalTests.java
+++ src/test/java/ru/netology/FunctionalTests.java
@@ -18,21 +18,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FunctionalTests {
 
+    public static final int INITIAL_AMOUNT = 1000;
+    public static final int RATE_FOR_ACCRUAL_PERIOD = 20;
+    public static final int PERIOD = 12;
+
     @Test
     void monthly_payment_calculation() {
-        BigDecimal monthlyPayment = CreditCalculator.monthlyPayment(1000, 20, 12);
+        BigDecimal monthlyPayment = CreditCalculator.monthlyPayment(INITIAL_AMOUNT, RATE_FOR_ACCRUAL_PERIOD, PERIOD);
         assertEquals("92.6345058970802", monthlyPayment.toString());
     }
 
     @Test
     void calculation_of_the_total_amount_to_be_returned_to_the_bank() {
-        BigDecimal totalAmount = CreditCalculator.totalAmountPayment(1000, 20, 12);
+        BigDecimal totalAmount = CreditCalculator.totalAmountPayment(INITIAL_AMOUNT, RATE_FOR_ACCRUAL_PERIOD, PERIOD);
         assertEquals("1111.6140707649624", totalAmount.toString());
     }
 
     @Test
     void calculation_of_overpayment_for_the_whole_period() {
-        BigDecimal overpayment = CreditCalculator.overpayment(1000, 20, 12);
+        BigDecimal overpayment = CreditCalculator.overpayment(INITIAL_AMOUNT, RATE_FOR_ACCRUAL_PERIOD, PERIOD);
         assertEquals("111.6140707649624", overpayment.toString());
     }
 }
