1b8d62ccc46999b9599d036f3e4e2c8afe3f3ba1
Rishat Suleymanov <spk_irbis@mail.ru>
2021-03-22 00:02:27 +0300

    Refactoring tests and classes

--- src/main/java/ru/netology/CreditCalculator.java
+++ src/main/java/ru/netology/CreditCalculator.java
@@ -1,25 +1,36 @@
 package ru.netology;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CreditCalculator {
-    public static BigDecimal monthlyPayment(int initialAmount, int rateForAccrualPeriod, int period) {
 
-        return BigDecimal.valueOf(92.6345058970802);
+    public static final int SCALE_RATE = 13;
+
+    public static BigDecimal monthlyPayment(double initialAmount, double rateForAccrualPeriod, int period) {
+        BigDecimal interestRate = new BigDecimal(rateForAccrualPeriod)
+                .divide(new BigDecimal(period), SCALE_RATE, RoundingMode.HALF_UP);
+        BigDecimal x5 = interestRate.add(new BigDecimal(1));
+        BigDecimal x4 = x5.pow(period);
+        BigDecimal x3 = x4.subtract(new BigDecimal(1));
+        BigDecimal x2 = interestRate.divide(x3, SCALE_RATE, RoundingMode.HALF_UP);
+        BigDecimal x1 = interestRate.add(x2);
+        BigDecimal monthlyPayment = new BigDecimal(initialAmount).multiply(x1);
+        return monthlyPayment.setScale(SCALE_RATE, RoundingMode.HALF_UP);
     }
 
-    public static BigDecimal totalAmountPayment(int initialAmount, int rateForAccrualPeriod, int period) {
+    public static BigDecimal totalAmountPayment(double initialAmount, double rateForAccrualPeriod, int period) {
         BigDecimal totalAmount = new BigDecimal(0);
         totalAmount = totalAmount
                 .add(monthlyPayment(initialAmount, rateForAccrualPeriod, period)
                         .multiply(BigDecimal.valueOf(period)));
-        return totalAmount;
+        return totalAmount.setScale(SCALE_RATE, RoundingMode.HALF_UP);
     }
 
-    public static BigDecimal overpayment(int initialAmount, int rateForAccrualPeriod, int period) {
+    public static BigDecimal overpayment(double initialAmount, double rateForAccrualPeriod, int period) {
         BigDecimal overpayment;
         overpayment = totalAmountPayment(initialAmount, rateForAccrualPeriod, period)
                 .subtract(new BigDecimal(initialAmount));
-        return overpayment;
+        return overpayment.setScale(SCALE_RATE, RoundingMode.HALF_UP);
     }
 }
